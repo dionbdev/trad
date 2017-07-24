378d6d8bb2fc960b31eeb16a557d0783d7545791
d bailey <d bailey>
2017-07-24 19:08:42 -0400

    added show button to comment

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     tallyRoutes = require("./routes/tally"),
     indexRoutes      = require("./routes/index")
     
-//mongoose.connect("mongodb://localhost/tally_test718");
-mongoose.connect("mongodb://demoDB:lamp@ds113063.mlab.com:13063/trad0719");
+mongoose.connect("mongodb://localhost/tally_test718");
+//mongoose.connect("mongodb://demoDB:lamp@ds113063.mlab.com:13063/trad0719");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -55,13 +55,13 @@ app.use("/tally/:id/comments", commentRoutes);
 
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The Server Has Started!");
-});
-
-
-// app.listen(4000, function(){
-//    console.log("The Tally Server Has Started! 4000");
+// app.listen(process.env.PORT, process.env.IP, function(){
+//    console.log("The Server Has Started!");
 // });
+
+
+app.listen(4000, function(){
+   console.log("The Tally Server Has Started! 4000");
+});
 
 
--- routes/comments.js
+++ routes/comments.js
@@ -82,4 +82,28 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
     });
 });
 
+
+// COMMENT SHOW ROUTE
+router.get("/:comment_id/show", middleware.checkCommentOwnership, function(req, res){
+   Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err){
+          res.redirect("back");
+      } else {
+        res.render("comments/show", {tally_id: req.params.id, comment: foundComment});
+      }
+   });
+});
+
+// COMMENT UPDATE
+// router.put("/:comment_id", middleware.sanitizeHtml, middleware.checkCommentOwnership, function(req, res){
+//    req.body.blog.body = req.sanitize(req.body.blog.body)
+//    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+//       if(err){
+//           res.redirect("back");
+//       } else {
+//           res.redirect("/tally/" + req.params.id );
+//       }
+//    });
+// });
+
 module.exports = router;
