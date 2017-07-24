7ee9c25908ae631ff3dc94bf2d8d7010e5416361
d bailey <d bailey>
2017-07-24 17:18:54 -0400

    Changed Port toe process

--- app.js
+++ app.js
@@ -55,13 +55,13 @@ app.use("/tally/:id/comments", commentRoutes);
 
 
 
-// app.listen(process.env.PORT, process.env.IP, function(){
-//    console.log("The Server Has Started!");
-// });
-
-
-app.listen(4000, function(){
-   console.log("The Tally Server Has Started! 4000");
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The Server Has Started!");
 });
+
+
+// app.listen(4000, function(){
+//    console.log("The Tally Server Has Started! 4000");
+// });
 
 
